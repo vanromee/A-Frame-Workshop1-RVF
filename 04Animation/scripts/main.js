a2965fb8bdab466a098bf91ddf2d4acb409f9efe
vanromee <58208601+vanromee@users.noreply.github.com>
2021-05-17 17:44:27 +0200

    correct counterclock?

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
