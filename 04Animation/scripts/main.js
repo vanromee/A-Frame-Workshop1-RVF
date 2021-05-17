c66e9874532a180b9f3acaba653d01732ea03328
vanromee <58208601+vanromee@users.noreply.github.com>
2021-05-17 17:50:18 +0200

    change back

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,7 +5,7 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-myOtherBox.object3D.rotation.x =+ rotationSpeed;
+myOtherBox.object3D.rotation.x += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
 }
 
